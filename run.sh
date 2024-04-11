02c676d25a8be5219ff8183c61293758b91db580
Ron Turetzky <turetzkyron@gmail.com>
2024-04-12 00:31:52 +0300

    chore: adding set-alias to the run script

--- run.sh
+++ run.sh
@@ -20,7 +20,7 @@ if [ "$1" == "encrypt" ]; then
     echo "EO_BLS_PRIVATE_KEY=$3" >> .private_key
     docker run --env-file .env --env-file .private_key --platform linux/amd64 ${private_path} ${EO_OPERATOR_CLI_IMAGE} ${1}
     rm .private_key > /dev/null 2>&1
-elif [ "$1" == "register" ] || [ "$1" == "deregister" ] || [ "$1" == "print-status" ] || [ "$1" == "decrypt" ] || [ "$1" == "generate-bls-key" ]; then
+elif [ "$1" == "register" ] || [ "$1" == "deregister" ] || [ "$1" == "print-status" ] || [ "$1" == "decrypt" ] || [ "$1" == "generate-bls-key" ] || [ "$1" == "eochain-set-alias" ]; then
     _cmd=$1
     shift
     docker run --env-file .env ${private_path} --platform linux/amd64 ${EO_OPERATOR_CLI_IMAGE} "${_cmd} $@"
